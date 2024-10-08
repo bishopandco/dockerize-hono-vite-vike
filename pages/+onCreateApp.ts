735e7db4120e402f91cac082edcab2fb88ebb76d
Bati <no-reply@batijs.dev>
2024-10-08 18:01:57 -0500

    scaffold Vike app with Bati

---
+++ pages/+onCreateApp.ts
@@ -0,0 +1,13 @@
+import type { OnCreateAppSync } from "vike-vue/types";
+import VueGtag from "vue-gtag";
+
+export const onCreateApp: OnCreateAppSync = (pageContext): ReturnType<OnCreateAppSync> => {
+  const { app } = pageContext;
+
+  // See https://matteo-gabriele.gitbook.io/vue-gtag/
+  app.use(VueGtag, {
+    config: {
+      id: import.meta.env.PUBLIC_ENV__GOOGLE_ANALYTICS,
+    },
+  });
+};
